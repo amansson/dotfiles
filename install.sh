1141ae981c95f26e2e89915eba0ffd55f9dbe34a
Andreas Månsson <andreas.mansson1@ingka.ikea.com>
2022-08-02 10:59:36 +0200

    add screensaver and end message

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ chmod +x scripts/*.sh
 scripts/install_brew.sh
 scripts/clean_dock.sh
 scripts/osx_tweaks.sh
+
+if ask "$os: Download the Aerial screensaver?" Y; then
+    brew cask install aerial
+fi
+
+echo "${green}All done!${normal}" | indent
