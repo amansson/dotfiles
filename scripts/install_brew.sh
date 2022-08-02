1ac13a7de17e692de1c33d730a4bbc482f550b51
Andreas Månsson <andreas.mansson1@ingka.ikea.com>
2022-08-02 10:51:31 +0200

    add brew apps to install

--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -2,9 +2,21 @@
 
 # Install brew
 if test ! $(which brew); then
-  echo "Installing Homebrew."
+  echo "Installing Brew."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo
 fi
 
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
+echo "Brew installed"
+echo
+echo
+echo "Start installing Brew apps"
+
+brew update
+brew bundle
+brew cleanup -s
+
+echo
+echo "Brew apps finished installing"
