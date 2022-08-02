44d4ea22d6b3608bf1c0b443e55e0b1d21017c4c
Andreas Månsson <andreas.mansson1@ingka.ikea.com>
2022-08-02 11:07:40 +0200

    add more fixes to brew with shell

--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -7,12 +7,34 @@ if test ! $(which brew); then
   echo
 fi
 
+echo "Adding Brew to PATH"
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+echo
+
+# Get Brew directory
+BREW_PREFIX=$(brew --prefix)
+
+  # Fix permissions for Zsh compaudit
+chmod go-w $BREW_PREFIX/share
+
+# Ensure Brew Zsh is a valid shell option
+if ! cat /etc/shells | grep $BREW_PREFIX/bin/zsh > /dev/null; then
+  echo "Adding Brew Zsh to list of allowed shells."
+  sudo sh -c 'echo ${BREW_PREFIX}/bin/zsh >> /etc/shells'
+  echo
+fi
+
+# Ensure Brew Bash is a valid shell option
+if ! cat /etc/shells | grep $BREW_PREFIX/bin/bash > /dev/null; then
+  echo "Adding Brew Bash to list of allowed shells."
+  sudo sh -c 'echo ${BREW_PREFIX}/bin/bash >> /etc/shells'
+  echo
+fi
 
 echo "Brew installed"
 echo
 echo
-echo "Start installing Brew apps"
+echo "Start installing Brew apps ..."
 
 brew update
 brew bundle
