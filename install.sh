583667e335f9c9ca6d4b9bd8ca01333eb144edbd
Andreas Månsson <andreas.mansson1@ingka.ikea.com>
2022-08-02 11:39:00 +0200

    remove aerial as optional cask

--- install.sh
+++ install.sh
@@ -7,8 +7,4 @@ scripts/install_brew.sh
 scripts/clean_dock.sh
 scripts/osx_tweaks.sh
 
-if ask "$os: Download the Aerial screensaver?" Y; then
-    brew cask install aerial
-fi
-
 echo "${green}All done!${normal}" | indent
